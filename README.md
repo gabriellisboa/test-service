80d1d975d7d821cc70e97b42bb87a6dc985d06e1
Gabiroto <gabriel.sl@outlook.com>
2020-01-03 11:04:10 -0300

    API key to env file Readme improvement Name improvement

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Service used as a default service for infrastructure testing
 
 ## Run
+Create a file with the name "dev.env" with the content of `CAT_API_KEY=<YOUR_API_KEY>`on the root of the application and add an api key from:
+    https://thecatapi.com/
+after that, just run:
 `docker-compose up`
--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from flask import Flask, jsonify, request, render_template, Response
 
 from cats.cats import get_cat_images, get_cats_info
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "♥ ❤ ♥ Cabritinha ♥ ❤ ♥"
+    return "♥ ❤ ♥ Gabiroto ♥ ❤ ♥"
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -22,14 +21,14 @@ def fetch_cats_info():
     cat_facts = get_cats_info()
     return jsonify(cat_facts)
 
-@app.route('/api/cats/images', methods=['GET'])
-def get_images():
-    cat_image = get_cat_images('d336bdee-3fd9-4afc-aa84-ae9b2a0635fb')[0]['url']
+@app.route('/api/cats/image', methods=['GET'])
+def get_cat_image():
+    cat_image = get_cat_images(os.environ["CAT_API_KEY"])[0]['url']
     return render_template('cats.html', cat_img=cat_image)
 
 @app.route('/api/error/500', methods=['POST'])
 def generate_error():
-    error_message = {'Error' : 'This is an example'}
+    error_message = {'Error' : 'This is an example 500 error'}
     return Response(str(error_message), status=500, mimetype='application/json')
     
 
